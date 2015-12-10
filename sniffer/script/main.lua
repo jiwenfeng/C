9f97fe8f19b19c44f21cde14f302f4d71d44f971
wenfengji <490257094@qq.com>
2015-12-10 21:56:13 +0800

    delete file

--- sniffer/script/main.lua
+++
@@ -1,4 +0,0 @@
-function main(handler, data, len)
-	print(data)
-	return len
-end
--- sniffer/sniffer_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SNIFFER_CONFIG_H__
-#define __SNIFFER_CONFIG_H__
-
-int open_config_file(const char *path);
-void close_config_file();
-int get_config(const char *key);
-
-#endif
